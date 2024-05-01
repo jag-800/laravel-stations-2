00c25e842a229661853cfd5fb4b29144a3222fed
joyun <joyunji1119@gmail.com>
2024-05-01 14:56:08 +0900

    finish/問題02

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('practice', function () {
-    return response('practice');
-});
-
-Route::get('practice2', function () {
-                $test = "practice2";
-    return response($test);
-});
-
-Route::get('practice3', function () {
-    $test = "test";
-return response($test);
-});
+use App\Http\Controllers\PracticeController;
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
